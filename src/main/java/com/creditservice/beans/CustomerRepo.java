f2ae8dbe7f72e9e8193525f9d0fedc5b5de4dad8
MattAEnea <matthew.a.enea@gmail.com>
2019-06-20 22:05:04 -0400

    Added an annotation

--- src/main/java/com/creditservice/beans/CustomerRepo.java
+++ src/main/java/com/creditservice/beans/CustomerRepo.java
@@ -3,8 +3,8 @@ package com.creditservice.beans;
 import com.creditservice.beans.CreditScore;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepo extends CrudRepository<CreditScore, Integer>
-{
-	
-}
+{}
