ae50fb4709079b02eb6b6d2779ab92ca413e3a04
MattAEnea <matthew.a.enea@gmail.com>
2019-06-20 22:05:23 -0400

    Changed controller code

--- src/main/java/com/creditservice/controller/CreditScoreController.java
+++ src/main/java/com/creditservice/controller/CreditScoreController.java
@@ -1,19 +1,15 @@
 package com.creditservice.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;  
 
 import com.creditservice.beans.*;
 
-@Controller
+@RestController
 @RequestMapping(path="/ssn")
 public class CreditScoreController 
 {
@@ -21,23 +17,12 @@ public class CreditScoreController
 	private CustomerRepo customerRepo;
 	
 	@PostMapping(path="/creditscore")
-	public @ResponseBody String addNewScore(
-			@RequestParam String name, @RequestParam int ssn,
-			@RequestParam int creditScore)
+	public String findScore(
+			@RequestParam int ssn, ModelAndView mandv)
 	{
-		CreditScore cScore = new CreditScore();
-		cScore.setName(name);
-		cScore.setSsn(ssn);
-		cScore.setCreditScore(creditScore);
-		customerRepo.save(cScore);
-		return "Saved";
+		CreditScore cScore = customerRepo.findById(ssn).get();
+		mandv.addObject("score", cScore);
+		return "find-score";
 	}
-	
-	@RequestMapping(value="/display", method = RequestMethod.POST)
-	public @ResponseBody Optional<CreditScore> getBySsn(int ssn)
-	{
-		return customerRepo.findById(ssn);
-	}
-	
 	
 }
